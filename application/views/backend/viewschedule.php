464faae7e377dd5c84ece969c35e47c40ceb593b
Pooja <pthakare33@gmail.com>
2016-04-08 19:01:07 +0530

    changes

--- application/views/backend/viewschedule.php
+++ application/views/backend/viewschedule.php
@@ -10,7 +10,7 @@
 <th data-field="stadium">stadium</th>
 <th data-field="team1">Team1</th>
 <th data-field="team2">Team2</th>
-<!--<th data-field="bookticket">Book Ticket</th>-->
+<th data-field="season">Season</th>
 <!--<th data-field="timestamp">Timestamp</th>-->
 </tr>
 </thead>
@@ -25,6 +25,12 @@
 </div>
 <script>
 function drawtable(resultrow) {
+    if(resultrow.season==1){
+        resultrow.season="Season 3";
+    }
+    else if(resultrow.season==2){
+        resultrow.season="Season 4";
+    }
 return "<tr><td>" + resultrow.id + "</td><td>" + resultrow.stadium + "</td><td>" + resultrow.team1 + "</td><td>" + resultrow.team2 + "</td><td><a class='btn btn-primary btn-xs waves-effect waves-light blue darken-4 z-depth-0 less-pad' href='<?php echo site_url('site/editschedule?id=');?>"+resultrow.id+"' data-position='top' data-delay='50' data-tooltip='Edit'><i class='fa fa-pencil propericon'></i></a><a class='btn btn-danger btn-xs waves-effect waves-light red pad10 z-depth-0 less-pad' onclick=\"return confirm('Are you sure you want to delete?');\") href='<?php echo site_url('site/deleteschedule?id='); ?>"+resultrow.id+"' data-position='top' data-delay='50' data-tooltip='Delete'><i class='material-icons propericon'>delete</i></a></td></tr>";
 }
 generatejquery("<?php echo $base_url;?>");
