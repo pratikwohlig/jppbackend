45829e0989c328bdc764de28a0fad6ce205f059f
Vinod Beloshe <vinodbeloshe12@gmail.com>
2016-05-21 11:34:00 +0530

    changes for hindi input

--- application/views/backend/header.php
+++ application/views/backend/header.php
@@ -22,7 +22,7 @@
 
 
     <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
 </head>
 
 <body>
