661d8e4a58c523aded2e8a01e9faa3ba66ee27bc
Pooja <pthakare33@gmail.com>
2016-06-18 18:09:59 +0530

    mailer part

--- application/views/emailer/pantherclan.php
+++ application/views/emailer/pantherclan.php
@@ -1,24 +1,18 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
-        <meta content="telephone=no" name="format-detection"/>
-        <title>Jaipur Pink Panthers</title>
-        <link href="https://fonts.googleapis.com/css?family=Oswald:400,700" rel="stylesheet">
-        <style type="text/css" media="screen">
-            body {
-              font-family: 'Oswald', sans-serif;
-              -webkit-font-smoothing: antialiased;
-              -moz-osx-font-smoothing: grayscale;
-              padding: 0 !important;
-              margin: 0 !important;
-              display: block !important;
-              -webkit-text-size-adjust: none;
-
-            }
-        </style>
-    </head>
-    <body class="body" style="padding:0 !important; margin:0 !important; display:block !important; -webkit-text-size-adjust:none;">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta content="telephone=no" name="format-detection" />
+    <title>Jaipur Pink Panthers</title>
+  </head>
+  <body class="body" style="display: block !important; -webkit-text-size-adjust: none; font-family: 'Oswald', sans-serif; -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; margin: 0; padding: 0;"><style type="text/css">
+@font-face {
+font-family: 'Oswald'; font-style: normal; font-weight: 400; src: local('Oswald Regular'), local('Oswald-Regular'), url('https://fonts.gstatic.com/s/oswald/v11/Y_TKV6o8WovbUd3m_X9aAA.ttf') format('truetype');
+}
+@font-face {
+font-family: 'Oswald'; font-style: normal; font-weight: 700; src: local('Oswald Bold'), local('Oswald-Bold'), url('https://fonts.gstatic.com/s/oswald/v11/bH7276GfdCjMjApa_dkG6aCWcynf_cDxXwCLxiixG1c.ttf') format('truetype');
+}
+</style>
         <table style="border-collapse:collapse" border="0" cellpadding="0" cellspacing="0" width="600" bgcolor="#fff">
             <tbody>
                 <tr>
@@ -32,7 +26,7 @@
                               </tr>
                               <tr>
                                   <td align="center">
-                                      <p style="color: #fff;">
+                                      <p style="color: #fff;font-family: 'Oswald', sans-serif;">
                                           <b>Hey <?php echo $name;?>,</b>
                                           <br>
                                           <br>
