ced7ea1b089619aa0995db10b780d556920d31fa
Pooja <pthakare33@gmail.com>
2016-07-01 10:29:01 +0530

    show no changed

--- application/models/restapi_model.php
+++ application/models/restapi_model.php
@@ -143,13 +143,13 @@ ORDER BY CONCAT(`jpp_schedule`.`startdate`, ' ', `starttime`) ASC")->row();
     }
     public function getScheduleSeason4()
     {
-         $query=$this->db->query("SELECT `jpp_schedule`.`id`,`jpp_schedule`.`ishome`,`jpp_schedule`.`season`, `jpp_stadium`.`name` as `stadium`, `jpp_stadium`.`hname` as `hindistadium`, `jppteam1`.`name` as `team1`, `jppteam1`.`hname` as `hinditeam1`,`jppteam1`.`id` as `team1id`, `jppteam2`.`name` as `team2`,`jppteam2`.`hname` as `hinditeam2`,`jppteam2`.`id` as `team2id`,`jpp_schedule`.`bookticket`, `jpp_schedule`.`score1`, `jpp_schedule`.`score2`,substring(CONCAT(DATE_FORMAT(`jpp_schedule`.`startdate`, '%d %b %Y'), ', ', `starttime`), 1, length(CONCAT(DATE_FORMAT(`jpp_schedule`.`startdate`, '%d %b %Y'), ', ', `starttime`)) - 3) as `starttimedate` FROM `jpp_schedule` LEFT OUTER JOIN `jpp_stadium` ON `jpp_stadium`.`id`=`jpp_schedule`.`stadium` LEFT OUTER JOIN `jpp_team` as `jppteam1`ON `jppteam1`.`id`=`jpp_schedule`.`team1` LEFT OUTER JOIN `jpp_team` as `jppteam2`ON `jppteam2`.`id`=`jpp_schedule`.`team2` WHERE `jpp_schedule`.`score1`='' AND   `jpp_schedule`.`score2`='' AND CONCAT(`jpp_schedule`.`startdate`, ' ', `starttime`) > NOW() ORDER BY CONCAT(`jpp_schedule`.`startdate`, ' ', `starttime`) ASC")->result();
+         $query=$this->db->query("SELECT `jpp_schedule`.`id`,`jpp_schedule`.`ishome`,`jpp_schedule`.`season`, `jpp_stadium`.`name` as `stadium`, `jpp_stadium`.`hname` as `hindistadium`, `jppteam1`.`name` as `team1`, `jppteam1`.`hname` as `hinditeam1`,`jppteam1`.`id` as `team1id`, `jppteam2`.`name` as `team2`,`jppteam2`.`hname` as `hinditeam2`,`jppteam2`.`id` as `team2id`,`jpp_schedule`.`bookticket`, `jpp_schedule`.`score1`, `jpp_schedule`.`score2`,substring(CONCAT(DATE_FORMAT(`jpp_schedule`.`startdate`, '%d %b %Y'), ', ', `starttime`), 1, length(CONCAT(DATE_FORMAT(`jpp_schedule`.`startdate`, '%d %b %Y'), ', ', `starttime`)) - 3) as `starttimedate`,`jpp_schedule`.`showNumber` FROM `jpp_schedule` LEFT OUTER JOIN `jpp_stadium` ON `jpp_stadium`.`id`=`jpp_schedule`.`stadium` LEFT OUTER JOIN `jpp_team` as `jppteam1`ON `jppteam1`.`id`=`jpp_schedule`.`team1` LEFT OUTER JOIN `jpp_team` as `jppteam2`ON `jppteam2`.`id`=`jpp_schedule`.`team2` WHERE `jpp_schedule`.`score1`='' AND   `jpp_schedule`.`score2`='' AND CONCAT(`jpp_schedule`.`startdate`, ' ', `starttime`) > NOW() ORDER BY CONCAT(`jpp_schedule`.`startdate`, ' ', `starttime`) ASC")->result();
 
-         $i=2;
-         foreach ($query as $row) {
-           $row->showNumber=$i;
-           $i++;
-         }
+        //  $i=3;
+        //  foreach ($query as $row) {
+        //    $row->showNumber=$i;
+        //    $i++;
+        //  }
 
 
         return $query;
