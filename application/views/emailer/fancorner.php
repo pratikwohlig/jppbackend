cb6c36324328f09301fe780201178edd8d200030
Pooja <pthakare33@gmail.com>
2016-07-18 17:10:30 +0530

    new html

--- application/views/emailer/fancorner.php
+++ application/views/emailer/fancorner.php
@@ -1,19 +1,13 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-    <meta content="telephone=no" name="format-detection" />
-    <title>Jaipur Pink Panthers</title>
-  </head>
-  <body class="body" style="display: block !important; -webkit-text-size-adjust: none; font-family: 'Oswald', sans-serif; -webkit-font-smoothing: antialiased; -moz-osx-font-smoothing: grayscale; margin: 0; padding: 0;">
-  <style type="text/css">
-@font-face {
-font-family: 'Oswald'; font-style: normal; font-weight: 400; src: local('Oswald Regular'), local('Oswald-Regular'), url('https://fonts.gstatic.com/s/oswald/v11/Y_TKV6o8WovbUd3m_X9aAA.ttf') format('truetype');
-}
-@font-face {
-font-family: 'Oswald'; font-style: normal; font-weight: 700; src: local('Oswald Bold'), local('Oswald-Bold'), url('https://fonts.gstatic.com/s/oswald/v11/bH7276GfdCjMjApa_dkG6aCWcynf_cDxXwCLxiixG1c.ttf') format('truetype');
-}
-</style>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><META http-equiv="Content-Type" content="text/html; charset=utf-8"></head><body>
+
+
+
+
+
+
+  <div style="display:block!important;font-family:&#39;Oswald&#39;,sans-serif;margin:0;padding:0">
+
         <table style="border-collapse:collapse" border="0" cellpadding="0" cellspacing="0" width="600" bgcolor="#fff">
             <tbody>
                 <tr>
@@ -21,60 +15,36 @@ font-family: 'Oswald'; font-style: normal; font-weight: 700; src: local('Oswald 
                         <table style="border-collapse:collapse" border="0" cellpadding="0" cellspacing="0" bgcolor="#093647">
                             <tbody>
                                 <tr>
-                                    <td  colspan="2">
-                                        <img src="http://jaipurpinkpanthers.com/emailers/fancor/1.jpg">
+                                    <td colspan="2">
+                                        <img src="http://jaipurpinkpanthers.com/emailers/fancor/1a.jpg">
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td align="center" colspan="2">
-                                        <p style="color: #fff;font-family: 'Oswald', sans-serif;font-size: 18px;">
-                                            Hey <?php echo $name;?>,
-                                        </p>
+                                    <td>
+                                        <a href="http://jaipurpinkpanthers.com/wallpaper" target="_blank"><img src="http://jaipurpinkpanthers.com/emailers/fancor/2a.jpg"></a>
+                                    </td>
+                                    <td>
+                                        <a href="http://jaipurpinkpanthers.com/gallery" target="_blank"><img src="http://jaipurpinkpanthers.com/emailers/fancor/3a.jpg"></a>
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td  colspan="2">
-                                        <img src="http://jaipurpinkpanthers.com/emailers/fancor/2.jpg">
+                                    <td colspan="2">
+                                        <a href="http://jaipurpinkpanthers.com/ticket-merchandise" target="_blank"><img src="http://jaipurpinkpanthers.com/emailers/fancor/4a.jpg"></a>
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td >
-                                        <a href="https://in.bookmyshow.com/sports/pro-kabaddi/jaipur-pink-panthers/seat-layout/ET00042335" target="_blank"><img src="http://jaipurpinkpanthers.com/emailers/fancor/3.jpg"></a>
-                                    </td>
-                                    <td >
-                                        <a href="https://in.bookmyshow.com/sports/pro-kabaddi/jaipur-pink-panthers/seat-layout/ET00042336" target="_blank"><img src="http://jaipurpinkpanthers.com/emailers/fancor/4.jpg"></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td >
-                                        <a href="https://in.bookmyshow.com/sports/pro-kabaddi/jaipur-pink-panthers/seat-layout/ET00042338" target="_blank"><img src="http://jaipurpinkpanthers.com/emailers/fancor/5.jpg"></a>
-                                    </td>
-                                    <td >
-                                        <a href="https://in.bookmyshow.com/sports/pro-kabaddi/jaipur-pink-panthers/seat-layout/ET00042337" target="_blank"><img src="http://jaipurpinkpanthers.com/emailers/fancor/6.jpg"></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td  colspan="2">
+                                    <td colspan="2">
                                         <table style="border-collapse:collapse" border="0" cellpadding="0" cellspacing="0" bgcolor="#093647">
                                             <tbody>
                                                 <tr>
                                                     <td>
-                                                        <img src="http://jaipurpinkpanthers.com/emailers/fancor/sh1.jpg">
+                                                        <a href="https://play.google.com/store/apps/details?id=com.jaipurpinkpanthers.android" target="_blank"><img src="http://jaipurpinkpanthers.com/emailers/fancor/5a.jpg"></a>
                                                     </td>
                                                     <td>
-                                                        <a href="https://www.facebook.com/sharer/sharer.php?u=https://in.bookmyshow.com/sports/pro-kabaddi/jaipur-pink-panthers/?utm_source=facebook&utm_medium=referral&utm_campaign=fb_pro-kabaddi_jaipur_pinkpanther_290116&rel=T2ZmZXJSZWRpcmVjdA%3D%3D" target="_blank"><img src="http://jaipurpinkpanthers.com/emailers/fancor/sh2.jpg"></a>
+                                                        <img src="http://jaipurpinkpanthers.com/emailers/fancor/6a.jpg">
                                                     </td>
                                                     <td>
-                                                        <a href="https://twitter.com/intent/tweet?url=https://in.bookmyshow.com/sports/pro-kabaddi/jaipur-pink-panthers/?utm_source=twitter&utm_medium=referral&utm_campaign=tw_pro-kabaddi_jaipur_pinkpanther_290116&rel=T2ZmZXJSZWRpcmVjdA%3D%3D" target="_blank"><img src="http://jaipurpinkpanthers.com/emailers/fancor/sh3.jpg"></a>
-                                                    </td>
-                                                    <td>
-                                                        <a href="mailto:?body=https://in.bookmyshow.com/sports/pro-kabaddi/jaipur-pink-panthers/?utm_source=twitter&utm_medium=referral&utm_campaign=tw_pro-kabaddi_jaipur_pinkpanther_290116&rel=T2ZmZXJSZWRpcmVjdA%3D%3D" target="_blank"><img src="http://jaipurpinkpanthers.com/emailers/fancor/sh4.jpg"></a>
-                                                    </td>
-                                                    <td>
-                                                        <a href="sms:?&body=https://in.bookmyshow.com/sports/pro-kabaddi/jaipur-pink-panthers/?utm_source=facebook&utm_medium=referral&utm_campaign=fb_pro-kabaddi_jaipur_pinkpanther_290116&rel=T2ZmZXJSZWRpcmVjdA%3D%3D" target="_blank"><img src="http://jaipurpinkpanthers.com/emailers/fancor/sh5.jpg"></a>
-                                                    </td>
-                                                    <td>
-                                                        <img src="http://jaipurpinkpanthers.com/emailers/fancor/sh6.jpg">
+                                                        <a href="https://itunes.apple.com/in/app/id1084678272?mt=8" target="_blank"><img src="http://jaipurpinkpanthers.com/emailers/fancor/7a.jpg"></a>
                                                     </td>
                                                 </tr>
                                             </tbody>
@@ -82,42 +52,22 @@ font-family: 'Oswald'; font-style: normal; font-weight: 700; src: local('Oswald 
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td  colspan="2">
+                                    <td colspan="2">
                                         <a href="http://jaipurpinkpanthers.com/jpp-tv" target="_blank"><img src="http://jaipurpinkpanthers.com/emailers/fancor/7.jpg"></a>
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td  colspan="2">
-
-                                        <table style="border-collapse:collapse" border="0" cellpadding="0" cellspacing="0" bgcolor="#093647">
-                                            <tbody>
-                                                <tr>
-                                                    <td >
-                                                        <a href="https://play.google.com/store/apps/details?id=com.jaipurpinkpanthers.android" target="_blank"><img src="http://jaipurpinkpanthers.com/emailers/fancor/8.jpg"></a>
-                                                    </td>
-                                                    <td >
-                                                        <a href="https://itunes.apple.com/in/app/id1084678272?mt=8" target="_blank"><img src="http://jaipurpinkpanthers.com/emailers/fancor/9.jpg"></a>
-                                                    </td>
-
-                                                </tr>
-                                            </tbody>
-                                        </table>
-
-                                    </td>
-
-                                </tr>
-                                <tr>
-                                    <td  colspan="2">
+                                    <td colspan="2">
                                         <a href="http://jaipurpinkpanthers.com/invite-friends" target="_blank"><img src="http://jaipurpinkpanthers.com/emailers/fancor/10.jpg"></a>
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td  colspan="2">
+                                    <td colspan="2">
                                         <table style="border-collapse:collapse" border="0" cellpadding="0" cellspacing="0" bgcolor="#093647">
                                             <tbody>
                                                 <tr>
                                                     <td>
-                                                        <img src="http://jaipurpinkpanthers.com/emailers/fancor/14.jpg">
+                                                        <img src="http://jaipurpinkpanthers.com/emailers/fancor/8a.jpg">
                                                     </td>
                                                     <td>
                                                         <a href="https://www.facebook.com/JaipurPinkPanthers/" target="_blank"><img src="http://jaipurpinkpanthers.com/emailers/fancor/11.jpg"></a>
@@ -142,5 +92,6 @@ font-family: 'Oswald'; font-style: normal; font-weight: 700; src: local('Oswald 
             </tbody>
         </table>
 
-    </body>
-</html>
+    </div>
+
+</body></html>
