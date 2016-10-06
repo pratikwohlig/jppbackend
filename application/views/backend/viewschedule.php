8272f9e98143fd39f26594e20822691eaacfe37d
Pooja <pthakare33@gmail.com>
2016-10-06 12:56:13 +0530

    adfsg

--- application/views/backend/viewschedule.php
+++ application/views/backend/viewschedule.php
@@ -31,6 +31,9 @@ function drawtable(resultrow) {
     else if(resultrow.season==2){
         resultrow.season="Season 4";
     }
+    else if(resultrow.season==3){
+        resultrow.season="World Cup 2016";
+    }
 return "<tr><td>" + resultrow.id + "</td><td>" + resultrow.stadium + "</td><td>" + resultrow.team1 + "</td><td>" + resultrow.team2 + "</td><td>" + resultrow.season + "</td><td><a class='btn btn-primary btn-xs waves-effect waves-light blue darken-4 z-depth-0 less-pad' href='<?php echo site_url('site/editschedule?id=');?>"+resultrow.id+"' data-position='top' data-delay='50' data-tooltip='Edit'><i class='fa fa-pencil propericon'></i></a><a class='btn btn-danger btn-xs waves-effect waves-light red pad10 z-depth-0 less-pad' onclick=\"return confirm('Are you sure you want to delete?');\") href='<?php echo site_url('site/deleteschedule?id='); ?>"+resultrow.id+"' data-position='top' data-delay='50' data-tooltip='Delete'><i class='material-icons propericon'>delete</i></a></td></tr>";
 }
 generatejquery("<?php echo $base_url;?>");
